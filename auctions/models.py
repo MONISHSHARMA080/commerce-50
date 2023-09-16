0bf66b46f7a7b1a82ba6dbbf2c459d3500dbfdb8
monishsharma010 <144796854+monishsharma010@users.noreply.github.com>
2023-09-16 16:38:21 +0000

    added watchlist ot models

--- auctions/models.py
+++ auctions/models.py
@@ -17,12 +17,12 @@ class Listings(models.Model):
     title = models.CharField(max_length=64)
     description =  models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='user')
-    imageUrl = models.URLField()
+    imageUrl = models.URLField(blank=True , null=True)
     date_created = models.DateField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,  blank=False)
     price = models.FloatField(max_length=10, blank=False)
+    watchlist = models.ManyToManyField(User,blank=True ,null=True, related_name="l_watchlist")
 
 
     def __str__(self):
         return f"{self.title}"
-
